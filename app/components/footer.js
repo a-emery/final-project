46d5f0f97ade52a828f1696b3bad016eacb10a8b
Aaron Emery <aemery14@gmail.com>
2015-11-13 10:51:44 -0500

    Changed image sources to links to work with current webpack deployment

--- app/components/footer.js
+++ app/components/footer.js
@@ -8,7 +8,7 @@ const Footer = React.createClass({
         <p className="footerCopyrightLeft">&copy; 2015 Aaron Emery</p>
         <div className="footerCopyrightRight">
           <a href="http://www.trailapi.com"><p className="footerCopyrightRightTrailAPI">Copyright 2012 TrailAPI</p></a>
-          <img src="../../public/assets/images/weatherunderground.png" alt=""  className="footerCopyrightRightWU"/>
+          <img src="http://s27.postimg.org/gwaykko0j/weatherunderground.png" alt=""  className="footerCopyrightRightWU"/>
         </div>
 
       </footer>
